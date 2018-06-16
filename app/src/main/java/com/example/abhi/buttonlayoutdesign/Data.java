6af6bcac3a041ef06ddd27bad79c406579a7c057
abhi1349@gmail.com <Aads@656>
2018-06-16 18:21:20 +0530

    first

---
+++ app/src/main/java/com/example/abhi/buttonlayoutdesign/Data.java
@@ -0,0 +1,14 @@
+package com.example.abhi.buttonlayoutdesign;
+
+public class Data {
+    public String title;
+    public String description;
+    public int imageId;
+
+    Data(String title, String description, int imageId) {
+        this.title = title;
+        this.description = description;
+        this.imageId = imageId;
+    }
+
+}
